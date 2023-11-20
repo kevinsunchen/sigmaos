6028db4097390f28156696eb354de3694e1f955e
kschen <kschen@mit.edu>
2023-11-20 20:21:10 +0000

    trying to fix ip issues -- bind to private ip, advertise to public

--- container/ip.go
+++ container/ip.go
@@ -1,11 +1,17 @@
 package container
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net"
+	db "sigmaos/debug"
 	"strings"
 
 	sp "sigmaos/sigmap"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 )
 
 // Rearrange addrs so that first addr is in the realm as clnt.
@@ -89,6 +95,41 @@ func LocalInterface() (string, error) {
 	return "", fmt.Errorf("localInterface: not found")
 }
 
+// adapted from https://gist.github.com/nanmu42/9c8139e15542b3c4a1709cb9e9ac61eb
+var privateIPBlocks []*net.IPNet
+
+func init() {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			db.DPrintf(db.ALWAYS, "parse error on %q: %v", cidr, err)
+		}
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+}
+
+func isPrivateIP(ip net.IP) bool {
+	if ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
+		return true
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
 func localIPs() ([]net.IP, error) {
 	var ips []net.IP
 	ifaces, err := net.Interfaces()
@@ -120,7 +161,51 @@ func localIPs() ([]net.IP, error) {
 	return ips, nil
 }
 
-// XXX should find what outgoing ip is
+func PublicIP() (string, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		db.DPrintf(db.ALWAYS, "Could not load default config: %v", err)
+		return "", err
+	}
+
+	client := imds.NewFromConfig(cfg)
+	// client := imds.New(imds.Options{})
+
+	publicip, err := client.GetMetadata(context.TODO(), &imds.GetMetadataInput{
+		Path: "public-ipv4",
+	})
+
+	if err == nil {
+		defer publicip.Content.Close()
+		bytes, _ := io.ReadAll(publicip.Content)
+		// log.Printf("ee %v", publ)
+		db.DPrintf(db.ALWAYS, "Retrieved public IP: %v\n", string(bytes))
+		return string(bytes), nil
+	}
+
+	db.DPrintf(db.ALWAYS, "Unable to retrieve the public IP address from the EC2 instance: %s\n", err)
+
+	ips, err := localIPs()
+	if err != nil {
+		db.DPrintf(db.ALWAYS, "Error retrieving local IPs: %v", err)
+	}
+
+	// if we have a local ip in 10.10.x.x (for Cloudlab), prioritize that first
+	for _, i := range ips {
+		if !(isPrivateIP(i)) {
+			db.DPrintf(db.ALWAYS, "%v", i.String())
+			return i.String(), nil
+		}
+	}
+
+	if len(ips) == 0 {
+		return "", fmt.Errorf("LocalIP: no IP")
+	}
+
+	db.DPrintf(db.ALWAYS, "Available IP: %v", ips[len(ips)-1].String())
+	return ips[len(ips)-1].String(), nil
+}
+
 func LocalIP() (string, error) {
 	ips, err := localIPs()
 	if err != nil {
